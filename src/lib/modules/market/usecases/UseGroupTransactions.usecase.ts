3b21956df067237dfc723bb225df31b4f72e9d98
Rotour <rotour.dev@gmail.com>
2024-02-08 10:00:22 +0100

    Transaction history merging fix similar prices

--- src/lib/modules/market/usecases/UseGroupTransactions.usecase.ts
+++ src/lib/modules/market/usecases/UseGroupTransactions.usecase.ts
@@ -1,27 +1,35 @@
 import type { TransactionRecord } from '../dto/TransactionRecord.dto';
 
+const arePriceSimilar = (price1: number, price2: number): boolean => {
+	return Math.floor(price1/100) === Math.floor(price2/100);
+}
+
 export const UseGroupTransactions = () => ({
 	execut: (transactions: TransactionRecord[]): TransactionRecord[] => {
 		const allTransactions = [...transactions];
 		const groupedTransactions: TransactionRecord[] = [];
 		let currentGroup: TransactionRecord[] = [];
 
-		for(let i = 0; i < allTransactions.length; i++) {
+		for (let i = 0; i < allTransactions.length; i++) {
 			// If empty, start a new group
 			if (currentGroup.length === 0) {
 				currentGroup.push(allTransactions[i]);
 				continue;
 			}
 			// If the current transaction is the same type, resource and price as the group, add it to the group
-			if (currentGroup[0].type === allTransactions[i].type && currentGroup[0].resource === allTransactions[i].resource && currentGroup[0].price === allTransactions[i].price) {
+			if (
+				currentGroup[0].type === allTransactions[i].type &&
+				currentGroup[0].resource === allTransactions[i].resource &&
+				arePriceSimilar(currentGroup[0].price, allTransactions[i].price)
+			) {
 				currentGroup.push(allTransactions[i]);
 				continue;
 			}
 			// If the current transaction is different, close the group and start a new one
 			groupedTransactions.push({
 				...currentGroup[0],
-				amount: currentGroup.reduce((acc, curr) => acc + curr.amount, 0),
-			})
+				amount: currentGroup.reduce((acc, curr) => acc + curr.amount, 0)
+			});
 			currentGroup = [];
 			currentGroup.push(allTransactions[i]);
 		}
